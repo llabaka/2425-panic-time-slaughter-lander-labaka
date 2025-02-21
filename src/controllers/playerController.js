e9c953911e712e52bdc855979a547c0bcee92c3d
llabaka <lander.labaka@ikasle.aeg.eus>
2025-02-21 12:27:24 +0100

    [DEVELOP][ADD] Times get

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,25 +1,25 @@
 const playerService = require('../services/playerService');
 
 const getAllPlayers = async (req, res) => {
-    try {
-        const allPlayers = await playerService.getPlayers();
-        if (allPlayers.length === 0) {
-            console.log("NO HAY CHARACTERS");
-            
-            return res.status(404).send({ message: "Characters don't exist" });
-        }
-        res.send({ status: "OK", data: allPlayers });
-    } catch (error) {
-        res
-            .status(error?.status || 500)
-            .send({
-                status: "FAILED",
-                message: "Error al realizar la petición:",
-                data: { error: error?.message || error }
-            });
+  try {
+    const allPlayers = await playerService.getPlayers();
+    if (allPlayers.length === 0) {
+      console.log("NO HAY CHARACTERS");
+
+      return res.status(404).send({ message: "Characters don't exist" });
     }
+    res.send({ status: "OK", data: allPlayers });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({
+        status: "FAILED",
+        message: "Error al realizar la petición:",
+        data: { error: error?.message || error }
+      });
+  }
 }
 
 module.exports = {
-    getAllPlayers
+  getAllPlayers
 }
